7df9395f2bfc5cf991e3d8ce0e0851bccbfd4ff7
zth <286727179@qq.com>
2022-06-22 16:24:06 +0800

    login module finished

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,65 +1,93 @@
-$(function () {
-  //点击去注册的链接
-  $('#linke-reg').on('click', function () {
-    $('.login-box').hide()
-    $('.reg-box').show()
-  })
-  $('#linke-login').on('click', function () {
-    $('.reg-box').hide()
-    $('.login-box').show()
-  })
+$(() => {
 
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
-  form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function (value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
-      }
-    }
-  })
-  // 监听表单的注册提交事件
-  var layer = layui.layer
-  $('#form-reg').on('submit', function (e) {
-    e.preventDefault();
-    var data = {
-      username: $('#form-reg [name=username]').val(),
-      password: $('#form-reg [name=password]').val()
-    }
-    console.log(data);
-    $.post('/api/reguser', data, function (res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录')
-      $('#linke-login').click()
+    $('#link_reg').on('click', () => {
+        $('.login-box').hide();
+        $('.reg-box').show();
     })
-  })
-  //监听表单的登录提交事件
-  $('#form-login').submit(function (e) {
-    e.preventDefault()
-    $.ajax({
-      url: '/api/login',
-      method:'POST',
-      data: $(this).serialize(),
-      success: function (res) {
-        if (res.status !== 0) {
-          return layer.msg(res.message)
+    $('#link_login').on('click', () => {
+        $('.reg-box').hide();
+        $('.login-box').show();
+    })
+
+    let form = layui.form;
+    form.verify({
+        username: function(value, item) { //value：表单的值、item：表单的DOM对象
+            if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+                return '用户名不能有特殊字符';
+            }
+            if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+                return '用户名首尾不能出现下划线\'_\'';
+            }
+            if (/^\d+\d+\d$/.test(value)) {
+                return '用户名不能全为数字';
+            }
+
+            //如果不想自动弹出默认提示框，可以直接返回 true，这时你可以通过其他任意方式提示（v2.5.7 新增）
+            if (value === 'xxx') {
+                alert('用户名不能为敏感词');
+                return true;
+            }
         }
-        
-        layer.msg('登录成功')
-        localStorage.setItem('token', res.token)
-        location.href = '/index.html'
-      }
+
+        //我们既支持上述函数式的方式，也支持下述数组的形式
+        //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+        ,
+        pass: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repass: (value) => {
+            if ($('#pwd').val() !== value) {
+                return '两次密码不一致'
+            }
+        }
+    });
+
+    $('#form_login').on('submit', (e) => {
+        e.preventDefault();
+        const fd = new FormData(($('#form_login'))[0]);
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: {
+                username: fd.get('username'),
+                password: fd.get('password')
+            },
+            success: (res) => {
+                if (res.status !== 0) {
+                    show_msg(res);
+                } else {
+                    localStorage.setItem('token', res.token);
+                    location.href = '/index.html';
+                }
+
+            }
+        })
     })
-  })
+
+    $('#form_reg').on('submit', (e) => {
+        e.preventDefault();
+        const fd = new FormData(($('#form_reg'))[0]);
+        $.ajax({
+            method: 'POST',
+            url: '/api/reguser',
+            data: {
+                username: fd.get('username'),
+                password: fd.get('password')
+            },
+            success: (res) => {
+                show_msg(res);
+                if (res.status === 0) {
+                    $('.reg-box input').prop('value', '')
+                    $('link_login').click();
+                }
+
+            }
+        })
+    })
+
+    function show_msg(res) {
+        const layer = layui.layer;
+        layer.msg(res.message);
+    }
+
 })
